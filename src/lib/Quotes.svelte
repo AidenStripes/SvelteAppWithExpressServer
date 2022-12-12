bb48c6f47060afe77732b826d0a172e680e8ece2
AidenStripes <84007924+AidenStripes@users.noreply.github.com>
2022-12-12 12:10:33 +0100

    Update Quotes.svelte

--- src/lib/Quotes.svelte
+++ src/lib/Quotes.svelte
@@ -29,7 +29,22 @@
 
   <ul>
     {#each quotes as quote}
-      <li>{quote.author} said {quote.text}</li>
+      <li class="q">{quote.text} <br />
+          {quote.author}
+      </li>
     {/each}
   </ul>
 </div>
+
+<style>
+  .q {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    margin-bottom: 50px;
+    list-style-type: none;
+  }
+
+  .q:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+</style>
